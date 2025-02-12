ad53912f44312ae57a70cd5b2f15f8086bf8a63c
Patrick Solberg <patrick.oysteinsen@gmail.com>
2025-02-12 17:23:32 +0100

    feat: quick init paeg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
-      <h1 className="capitalize text-2xl">patrick solberg øystensen</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h1 className="capitalize text-2xl">patrick solberg øystensen</h1>
+      </div>
     </main>
   );
 }
