24a842702206219ed7d97886abfe9e4c09d51c2d
Patrick Solberg <patrick.oysteinsen@gmail.com>
2025-02-12 16:41:35 +0100

    chore: :wastebasket: black slate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
       <h1 className="capitalize text-2xl">patrick solberg øystensen</h1>
     </main>
   );
