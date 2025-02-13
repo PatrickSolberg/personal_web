1c127efb160cee223bb70faeac6bb45bf8213bc3
Patrick Solberg <patrick.oysteinsen@gmail.com>
2025-02-13 18:25:14 +0100

    feat: add frontpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1 className="capitalize text-2xl">patrick solberg øystensen</h1>
-      </div>
-    </main>
+    <section>
+      <p>{"Hello world! 👋. I'm Patrick. Welcome to my personal website."}</p>
+    </section>
   );
 }
