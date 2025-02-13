23d1fd150a663c0477231745a69931421b4d891b
Patrick Solberg <patrick.oysteinsen@gmail.com>
2025-02-13 20:08:57 +0100

    feat: :sparkles: add utils file

---
+++ src/app/components/utils.ts
@@ -0,0 +1,12 @@
+export const perInfo = {
+  name: "Patrick S. Øystensen",
+  title: "Software Engineer",
+  location: "Bergen, Norway",
+  email: "patrick.oystensen@gmail.com",
+  github: "https://github.com/patricksolberg",
+  gitRepo: "https://github.com/patricksolberg/personal_web",
+};
+
+export const perStatement = {
+  statement: `I'm an aspiring software engineer with a passion for building scalable and performant applications. I'm excited to explore new technologies and contribute to the open-source community.`,
+};
