dd5ffdb151014441b53c3d8d9a4a7c10714a9de7
Patrick Solberg <patrick.oysteinsen@gmail.com>
2025-02-13 23:47:37 +0100

    feat: add statment etc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
+import Link from "next/link";
 import { perInfo, perStatement } from "./components/utils";
 
 export default function Home() {
   return (
-    <section className="flex flex-col">
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        {`Hello world! 👋. I'm ${perInfo.name}`}
-      </h1>
-      <p className="mb-4">{perStatement.statement}</p>
-    </section>
+    <>
+      <section className="flex flex-col">
+        <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+          {`Hello world! 👋 I'm ${perInfo.name}`}
+        </h1>
+        <p className="mb-4">{perStatement.statement}</p>
+        <Link href="/cv">
+          <p>See my CV</p>
+        </Link>
+      </section>
+    </>
   );
 }
