d590317d0d9a48f7fbb46d7d25e9cd9c2fb70f09
Patrick Solberg <patrick.oysteinsen@gmail.com>
2025-02-13 20:08:35 +0100

    feat: add navlinks

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,20 +2,30 @@ import Link from "next/link";
 
 const navItems = {
   "/": {
-    name: "home",
+    name: "about me",
   },
   "/blog": {
     name: "blog",
+  },
+  "/contact": {
+    name: "contact",
   },
 };
 
 export default function Navbar() {
   return (
-    <aside>
-      <nav>
-        <div>
+    <aside className="-ml-[8px] mb-16 tracking-tight">
+      <nav
+        className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+        id="nav"
+      >
+        <div className="flex flex-row space-x-0 pr-10 capitalize">
           {Object.entries(navItems).map(([href, { name }]) => (
-            <Link key={href} href={href}>
+            <Link
+              key={href}
+              href={href}
+              className="transition-all flex align-middle relative py-1 px-2 m-1"
+            >
               {name}
             </Link>
           ))}
